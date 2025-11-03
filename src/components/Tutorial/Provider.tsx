d4e8e370637200731c6202520a202cfcf23a7b19
Chase Douglas <chasedouglas@gmail.com>
2025-11-03 10:55:39 -0800

    When restarting tutorials, jump to selection step
    This skips all the background context steps.

--- src/components/Tutorial/Provider.tsx
+++ src/components/Tutorial/Provider.tsx
@@ -308,21 +308,25 @@ const tutorialReducer: React.Reducer<TutorialState, TutorialReducerArg> = (state
       return newState;
     }
 
-    case 'restartTutorial':
+    case 'restartTutorial': {
       sessionStorage.removeItem('tutorialClosed');
       sessionStorage.setItem('tutorialName', Tutorial.Intro);
-      sessionStorage.setItem('tutorialStepIndex', '0');
+
+      const tutorialStep = tutorials[Tutorial.Intro].length - 1;
+
+      sessionStorage.setItem('tutorialStepIndex', tutorialStep.toString());
 
       posthog.capture('tutorial_restarted');
 
       return {
         ...state,
         tutorialName: Tutorial.Intro,
-        stepIndex: 0,
-        currentStep: getNextStep(Tutorial.Intro, 0),
+        stepIndex: tutorialStep,
+        currentStep: getNextStep(Tutorial.Intro, tutorialStep),
         closed: false,
         atEnd: false,
       };
+    }
 
     case 'setElementRef':
       return { ...state, refs: { ...state.refs, [arg.name]: arg.value } };
